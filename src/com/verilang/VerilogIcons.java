49e33726fee939d858b3185adbb4b04c6bd8ed35
mrtsepa <tsepa.stas@gmail.com>
2018-06-02 18:14:18 +0300

    move icons folder

--- src/com/verilang/VerilogIcons.java
+++ src/com/verilang/VerilogIcons.java
@@ -6,6 +6,6 @@ import javax.swing.Icon;
 
 public class VerilogIcons {
 
-    public static final Icon ICON = IconLoader.getIcon("/com/verilang/icons/icon.png");
+    public static final Icon ICON = IconLoader.getIcon("/icons/icon.png");
 
 }
